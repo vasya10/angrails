0aaf92d79491035b257292a7e0c4feec68121167
vasya10 <vasya10@gmail.com>
2014-03-16 13:02:27 -0500

    add angular tests for starCatalogCtrl

--- test/javascript/unit/controllersSpec.js
+++ test/javascript/unit/controllersSpec.js
@@ -4,7 +4,7 @@ describe('angrailsTest', function () {
 
 	beforeEach(module('angrailsApp'));
 
-	var scope, mainCtrl;
+	var scope, mainCtrl, starCatalogCtrl;
   
 	beforeEach(inject(function ($compile, $rootScope) {
 		scope = $rootScope.$new();
@@ -16,12 +16,36 @@ describe('angrailsTest', function () {
 			mainCtrl = $controller("MainCtrl", {$scope: scope});
 		}));
 
-
 		it("should set hello text", function () {
 			var helloText = 'Hello Angrails Demo!';
 			expect(scope.helloText).toEqual(helloText);
 		});
+	});
 
+	describe('angrailsStarCatalogCtrl', function () {
 
+		var httpBackend;
+
+		beforeEach(inject(function ($controller) {
+			starCatalogCtrl = $controller("StarCatalogCtrl", {$scope: scope});
+		}));
+
+		beforeEach(inject(function ($httpBackend) {
+			httpBackend = $httpBackend;
+			$httpBackend.whenGET('/angrails/starCatalog').respond({
+					name: 'Aldebaran',
+					distance: 65
+			});
+		}));
+
+		it("should retrieve star catalog", function () {
+			var expected = {
+				name: 'Aldebaran',
+				distance: 65
+			};
+			httpBackend.flush();
+			expect(scope.starCatalog).toEqual(expected);
+		});
 	})
+
 });
